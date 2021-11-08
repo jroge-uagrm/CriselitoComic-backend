0ffec0930ad16b76e272f17e7cbe1af735af68f2
Jorge Rodrigo Torrez Aramayo <jorgerodrigotorrez@gmail.com>
2021-11-08 07:54:28 -0400

    Hardcoding values, CORS policy added

--- Program.cs
+++ Program.cs
@@ -13,7 +13,15 @@ builder.Services.Configure<CloudinarySettings>(cloudinarySettings);
 builder.Services.AddSingleton<CloudinarySettings>(sp =>
                 sp.GetRequiredService<IOptions<CloudinarySettings>>().Value);
 
-builder.Services.AddSingleton<ComicsRepository>(); 
+builder.Services.AddSingleton<ComicsRepository>();
+
+builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +31,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("MyPolicy");
 
 app.Run();
