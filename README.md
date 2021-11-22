381f216ae178ed7013fbcf96e7f4b73e9b3740cf
Jorge Rodrigo Torrez Aramayo <jorgerodrigotorrez@gmail.com>
2021-11-22 02:38:24 +0000

    project imported from a template

---
+++ README.md
@@ -0,0 +1,11 @@
+Clone repo: git clone https://github.com/cambasoft/webservices
+
+Move inside project: cd webservices
+
+Install dependencies: composer install
+
+Copy file `.env.example` in a new file called `.env`
+
+Generate app key: php artisan key:generate
+
+Run project: php artisan serve
---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function responseOk($data)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Done.',
+            'data' => $data
+        ], 200);
+    }
+
+    public function responseCreated($data)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Created Successfully.',
+            'data' => $data
+        ], 201);
+    }
+
+    public function responseBadRequest($data)
+    {
+        return response()->json([
+            'status' => 'warning',
+            'message' => 'Wrong Information.',
+            'data' => $data
+        ], 400);
+    }
+
+    public function responseNotFound($data)
+    {
+        return response()->json([
+            'status' => 'warning',
+            'message' => 'Not Found.',
+            'data' => $data
+        ], 404);
+    }
+
+    public static function responseDataBaseError($message, $data = null)
+    {
+        return response()->json([
+            'status' => 'db_warning',
+            'message' => $message,
+            'data' => $data
+        ], 406);
+    }
+
+    public static function responseInternalError($message, $data = null)
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+            'data' => $data
+        ], 500);
+    }
+}
