5815cc67e264bdef622115791ccdfb8fd70918f0
Jorge Rodrigo Torrez Aramayo <jorgerodrigotorrez@gmail.com>
2021-11-23 10:40:14 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->group(function () {
     Route::get('echo', function () {
-        return "Cambasoft Web Services running...";
+        return "CriselitoComic-backend Web Services running...";
     });
 
     Route::apiResources([
